f19bc44b31d84995fc12e59dfdc9ed677b23a262
jian <loveinansan1@gmail.com>
2024-07-30 14:13:33 +0900

    #9 김영한의 기초 자바 secondClass 논리 연산자 & 활용
    && ( 그리고)
    || (또는)
    ! ( 부정)

---
+++ src/operator/Logical2.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Logical2 {
+    public static void main(String[] args) {
+        int a = 15;
+        // a 는 10보다 크고 20보다 작다
+        boolean result = a > 10 && a < 20; //(a > 10) && (a < 20)
+        boolean result2 = 10 < a && a < 20; //(a > 10) && (a < 20)
+        System.out.println("result = " + result);
+        System.out.println("result2 = " + result2);
+    }
+}
