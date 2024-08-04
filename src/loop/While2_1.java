eb0edaf58dbb2fd3b00622abf74307eb5660a5a0
jian <loveinansan1@gmail.com>
2024-08-04 19:41:16 +0900

    #17 김영한의 기초 자바 fourthClass cond
    - do-while

--- src/loop/While2_1.java
+++ src/loop/While2_1.java
@@ -5,15 +5,13 @@ public class While2_1 {
         int sum = 0;
         int i = 10;
 
-        sum = sum + i;
-        System.out.println("i=" + i + " sum=" + sum);
-        i++;
+        sum = sum + 1;
+        System.out.println("i=" + 1 + " sum=" + sum);
 
-        sum = sum + i;
-        System.out.println("i=" + i + " sum=" + sum);
-        i++;
+        sum = sum + 2;
+        System.out.println("i=" + 2 + " sum=" + sum);
 
-        sum = sum + i;
-        System.out.println("i=" + i + " sum=" + sum);
+        sum = sum + 3;
+        System.out.println("i=" + 3 + " sum=" + sum);
     }
 }
