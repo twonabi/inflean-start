f8096e584d936ad007b7df486ed5ecd1063621cd
jian <loveinansan1@gmail.com>
2024-08-07 17:01:19 +0900

    #24 training_scanner_ex
    4번 문제
    잘못되서 다시 커밋.

--- src/scanner/ex/ScannerWhileEx4.java
+++ src/scanner/ex/ScannerWhileEx4.java
@@ -4,30 +4,39 @@ import java.util.Scanner;
 
 public class ScannerWhileEx4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        int totalCost = 0;
 
-        int sum = 0;
-        int count = 0;
-        int input = 0;
+        while (true) {
+            System.out.println("1: 상품 입력, 2: 결제, 3: 프로그램 종료");
+            int option = input.nextInt();
 
-        System.out.println("1: 상품입력, 2: 결제, 3: 프로그램 종료");
+            if ( option == 1) {
+                input.nextLine();
 
-      /*  while (true) {
-            input = scanner.nextInt();
-            if (input == -1) {
+                System.out.println("상품명을 입력하세요: ");
+                String product = input.nextLine();
+
+                System.out.println("상품의 가격을 입력하세요: ");
+                int price = input.nextInt();
+
+                System.out.println("구매 수량을 입력하세요: ");
+                int quantity = input.nextInt();
+
+                totalCost += price * quantity;
+                System.out.println("상품명: " + product + " 가격:" + price + " 수량:" + quantity + " 합계:" + price * quantity);
+
+            } else if (option == 2) {
+                System.out.println("총 비용: " + totalCost);
+                totalCost = 0;
+
+            } else if (option == 3) {
+                System.out.println("프로그램을 종료합니다");
                 break;
+
+            }else {
+                System.out.println("올바른 옵션을 선택 해주세요.");
             }
-            sum += input;
-            count++;
-        }*/
-
-        while ((input = scanner.nextInt()) != -1) {
-            sum += input;
-            count++;
         }
-
-        double average = (double) sum / count;
-        System.out.println("입력한 숫자들의 합계: " + sum);
-        System.out.println("입력한 숫자들의 평균: " + average);
     }
 }
